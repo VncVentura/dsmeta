f3ced28010621fd2137ca64d697297c8e1e94e02
FL0P1 <ribeiro.vini69@gmail.com>
2022-11-18 15:26:25 -0300

    Date Select

--- backend/src/main/java/com/devsuperior/dsmeta/services/SalesService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SalesService.java
@@ -1,6 +1,8 @@
 package com.devsuperior.dsmeta.services;
 
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -16,10 +18,13 @@ public class SalesService {
 	@Autowired
 	private SaleRepository repository;
 
-	public Page<Sales> findSales(String minDate, String maxDate,Pageable pageable) {
-		LocalDate min = LocalDate.parse(minDate);
-		LocalDate max = LocalDate.parse(maxDate);
-		
-		return repository.findSales(min,max,pageable);
+	public Page<Sales> findSales(String minDate, String maxDate, Pageable pageable) {
+
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+
+		LocalDate min = minDate.equals("") ? today.minusYears(365): LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+
+		return repository.findSales(min, max, pageable);
 	}
 }
