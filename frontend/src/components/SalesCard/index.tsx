3d35e7cd1daf5ef57aedb8567c221b5a987917ac
FL0P1 <ribeiro.vini69@gmail.com>
2022-11-20 20:31:43 -0300

    Axios

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -2,16 +2,24 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import './styles.css'
 import NotificationButton from "../NotificationButton"
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function SalesCard() {
 
     const min = new Date(new Date().setDate(new Date().getDate() - 365));
-    const max =  new Date();
+    const max = new Date();
 
     const [mindDate, setMinDate] = useState(min);
 
     const [maxdDate, setMaxDate] = useState(max);
+
+    useEffect(() => { 
+        axios.get("http://localhost:8080/sales")
+        .then(reponse =>{
+            console.log(reponse.data)
+        })
+    }, []);
 
 
     return (
